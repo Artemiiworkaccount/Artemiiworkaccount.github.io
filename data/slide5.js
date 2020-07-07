21caea5ceae997537802a05e3653c0de03bf48e0
Artemiiworkaccount <67731468+Artemiiworkaccount@users.noreply.github.com>
2020-07-07 12:17:37 +0300

    Add files via upload

---
+++ data/slide5.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{1534B516-9EB7-4197-8747-49593A745152}'];loadHandler&&loadHandler(4, '<div id="spr0_7fd69"><div id="spr1_7fd69" class="kern slide"><img id="img0_7fd69" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_7fd69" class="kern slide"></div></div>');})();
---
+++ data/slide7.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{1534B516-9EB7-4197-8747-49593A745152}'];loadHandler&&loadHandler(6, '<div id="spr0_7fff9"><div id="spr1_7fff9" class="kern slide"><img id="img0_7fff9" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_7fff9" class="kern slide"></div></div>');})();
